8e33107d631e634606b97ac7990accca793a5620
Vinicius Campos <viniciuscampos4@gmail.com>
2025-05-02 00:57:41 -0300

    feat: implement reusable modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,17 @@
+import { createPortal } from 'react-dom';
+import { Button } from '../Shared/Button';
+const Modal = ({ children, ref, buttonCaption }) => {
+  return createPortal(
+    <dialog
+      ref={ref}
+      className="backdrop: bg-stone-900/90 p-4 rounded-md shadow-md"
+    >
+      {children}
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
+      </form>
+    </dialog>,
+    document.getElementById('modal-root')
+  );
+};
+export default Modal;
