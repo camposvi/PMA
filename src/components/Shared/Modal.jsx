315a23d0f765ed461ac43733fda69c1872e05d64
Vinicius Campos <viniciuscampos4@gmail.com>
2025-05-02 01:09:51 -0300

    fix: resolve button import error

--- src/components/Shared/Modal.jsx
+++ src/components/Shared/Modal.jsx
@@ -1,5 +1,5 @@
 import { createPortal } from 'react-dom';
-import { Button } from './Button';
+import Button from '../Shared/Button';
 const Modal = ({ children, ref, buttonCaption }) => {
   return createPortal(
     <dialog
